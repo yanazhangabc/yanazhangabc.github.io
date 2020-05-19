dc64fc5ba0cd9f931e49faba5a7a5a10306c9ac5
张海彦 <yanazhangabc@126.com>
2020-05-19 18:42:30 +0800

    hah

---
+++ js/index.js
@@ -0,0 +1,43 @@
+// 获取目标元素
+var closeBtn = document.getElementById('close_btn')
+var maskLayer = document.getElementsByClassName('mask-layer')[0]
+var open1 = document.getElementById('open')
+var open = document.getElementById('open1')
+var download = document.getElementById('download')
+
+// 判断设备类型
+var ua = window.navigator.userAgent
+var isAndroid = ua.indexOf('Android') > -1 || ua.indexOf('Adr') > -1;
+
+// 点击关闭提示层
+closeBtn.onclick = function () {
+    maskLayer.style.display = 'none'
+}
+// 打开App
+if (ua.indexOf('MicroMessenger') === -1) { // 非微信端
+    window.location.href = "com.cebbank.xyk://"
+} else {
+    maskLayer.style.display = 'block'
+}
+
+// 下载App
+download.onclick = function () {
+    if (!isAndroid) {
+        window.location.href = "http://itunes.apple.com/cn/app/id794862904"
+    } else {
+        window.location.href = "https://yghsh.cebbank.com/static/download/APPphone/apk/ebank_mobile_card.apk?ver=5.0.5";
+    }
+}
+// 打开App
+open.onclick = function () {
+    if (!isAndroid) {
+        window.location.href = "com.cebbank.xyk://"
+        window.location.href = "http://itunes.apple.com/cn/app/id794862904"
+    } else {
+        window.location.href = "https://yghsh.cebbank.com/static/download/APPphone/apk/ebank_mobile_card.apk?ver=5.0.5";
+    }
+}
+open1.onclick = function () {
+    window.location.href = 'https://yanazhangabc.github.io'
+}
+
