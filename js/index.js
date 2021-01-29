9e77e730db52982e0986c362dda8e7aa70822c08
张海彦 <yanazhangabc@126.com>
2021-01-29 14:12:53 +0800

    2

--- js/index.js
+++ js/index.js
@@ -1,18 +1,5 @@
 // 获取目标元素
-var closeBtn = document.getElementById('close_btn')
-var maskLayer = document.getElementsByClassName('mask-layer')[0]
-
 var open1 = document.getElementById('open1')
-
-// 判断设备类型
-var ua = window.navigator.userAgent
-var isAndroid = ua.indexOf('Android') > -1 || ua.indexOf('Adr') > -1;
-
-// 点击按钮
-//open1.onclick = function () {
-//    window.location.href = 'https://cwma.cebbank.com/applinkUL'
-//}
-
 ULink.start({
   id: 'usruisrboasjdhp', /* 平台为每个应用分配的方案link ID，必填 */
   data: {} /* 自定义参数，选填 */
