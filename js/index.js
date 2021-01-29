6fba67bcfacaaca1a498606defbd050732c39c79
张海彦 <yanazhangabc@126.com>
2021-01-29 11:34:26 +0800

    feat：DeepLink打开App

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 // 获取目标元素
 var closeBtn = document.getElementById('close_btn')
 var maskLayer = document.getElementsByClassName('mask-layer')[0]
-var open = document.getElementById('open')
+
 var open1 = document.getElementById('open1')
-var download = document.getElementById('download')
 
 // 判断设备类型
 var ua = window.navigator.userAgent
@@ -12,39 +11,6 @@ var isAndroid = ua.indexOf('Android') > -1 || ua.indexOf('Adr') > -1;
 // 点击关闭提示层
 closeBtn.onclick = function () {
     maskLayer.style.display = 'none'
-}
-// 打开App
-if (ua.indexOf('MicroMessenger') === -1) { // 非微信端
-    // window.location.href = "com.cebbank.xyk://"
-} else {
-    // maskLayer.style.display = 'block'
-}
-
-// 下载App
-download.onclick = function () {
-    if (!isAndroid) {
-        // window.location.href = "http://itunes.apple.com/cn/app/id794862904"
-    } else {
-        // window.location.href = "https://yghsh.cebbank.com/static/download/APPphone/apk/ebank_mobile_card.apk?ver=5.0.5";
-    }
-    window.location.href = "http://itunes.apple.com/cn/app/id794862904"
-}
-// 打开App
-open.onclick = function () {
-    if (!isAndroid) {
-        //window.location.href = "com.smoothzhang.WechatDemo://jump"
-        // window.location.href = "http://itunes.apple.com/cn/app/id794862904"
-    } else {
-        //window.location.href = "https://yghsh.cebbank.com/static/download/APPphone/apk/ebank_mobile_card.apk?ver=5.0.5";
-    }
-    
-    window.location.href = 'https://yanazhangabc.github.io/app/'
-}
-// linkUL打开App
-open1.onclick = function () {
-    // window.location.href = 'https://zyzmlc.github.io/'
-    // window.location.href = 'https://sifuweifang.com/'
-    window.location.href = 'https://cwma.cebbank.com/applinkUL'
 }
 
 ULink.start({
