a5662361b1cfae1ddc75d57dc0ed108c32053058
张海彦 <yanazhangabc@126.com>
2021-03-01 16:04:57 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -1,15 +1,9 @@
-/*
-* MobLink 支持数组=>MobLink([...]) 和对象=>MobLink({...}) 两种初始化形式
-* 页面上有多个元素需要跳转时使用数组方式,仅单个元素需要跳转时可以使用对象的方式进行初始化
-* el: 表示网页上Element的id值,该字段为空或者不写,则表示MobLink **默认浮层** 上的打开按钮(注意:该字段支持selector方式: 1.'.class' 2.'#id1' 3.['#id1','#id2'])
-* path: 对应App里需要恢复页面的路径
-* default: 指定分享页默认场景参数（最多只能指定一项作为默认场景参数）
-* params: 网页需要带给客户端的参数
-*/
 // 页面上仅单个元素需要跳转时可以使用对象方式进行初始化
 MobLink({
   el: '',
-  path: 'demo/a',
+  path: '/AboutQRViewController/a',
+moblink_source_ios: '6037a9a7b0a3571eaeceea7e',
+moblink_source_android: '60389adeb0a3571eaeceea81',
   default: true,
   params: {
       key1: 'value1',
@@ -21,7 +15,9 @@ MobLink({
 MobLink([
   {
       el: '',
-      path: 'demo/a',
+      path: '/AboutQRViewController/a',
+moblink_source_ios: '6037a9a7b0a3571eaeceea7e',
+moblink_source_android: '60389adeb0a3571eaeceea81',
       params: {
           key1: 'value1',
           key2: 'value2',
@@ -29,7 +25,9 @@ MobLink([
   },
   {
       el: '#openAppBtn1',
-      path: 'demo/b',
+      path: '/AboutQRViewController/a',
+moblink_source_ios: '6037a9a7b0a3571eaeceea7e',
+moblink_source_android: '60389adeb0a3571eaeceea81',
       params: {
           key1: 'value1',
           key2: 'value2',
@@ -37,10 +35,12 @@ MobLink([
   },
   {
       el: '#openAppBtn2',
-      path: 'demo/c',
+       path: '/AboutQRViewController/a',
+moblink_source_ios: '6037a9a7b0a3571eaeceea7e',
+moblink_source_android: '60389adeb0a3571eaeceea81',
       params: {
           key1: 'value1',
           key2: 'value2',
       }
   }
- ]);
+ ]);s
