3dac9951b5033edae649b4fff789fc95b7dba486
张海彦 <yanazhangabc@126.com>
2021-04-21 13:59:22 +0800

    qq

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // 页面上仅单个元素需要跳转时可以使用对象方式进行初始化
 MobLink({
   el: '',
-  path: '/CEBWKBaseViewController/a',
+  path: 'yghsh-ios/CEBCREDIT/CEBWKBaseViewController',
 moblink_source_ios: '607e2b1363c0332d6516cd87',
 moblink_source_android: '60389adeb0a3571eaeceea81',
   default: true,
@@ -11,34 +11,34 @@ moblink_source_android: '60389adeb0a3571eaeceea81',
   }
  });
 
- // 页面上有多个元素需要跳转时要使用数组方式进行初始化
-MobLink([
-  {
-      el: '',
-   path: '/MobLinkViewController/a',
-moblink_source_ios: '607e2b1363c0332d6516cd87',
-  default: true,
-  params: {
-      key1: 'https://yanazhangabc.github.io',
-      key2: 'value2',
-  }
-  },
-  {
-      el: '#openAppBtn1',
-      path: '/MobLinkViewController/a',
-moblink_source_ios: '607e2b1363c0332d6516cd87',
-      params: {
-          key1: 'value1',
-          key2: 'value2',
-      }
-  },
-  {
-      el: '#openAppBtn2',
-       path: '/MobLinkViewController/a',
-moblink_source_ios: '607e2b1363c0332d6516cd87',
-      params: {
-          key1: 'value1',
-          key2: 'value2',
-      }
-  }
- ]);
+// // 页面上有多个元素需要跳转时要使用数组方式进行初始化
+//MobLink([
+//  {
+//      el: '',
+//   path: 'yghsh-ios/CEBCREDIT/MobLinkViewController/a',
+//moblink_source_ios: '607e2b1363c0332d6516cd87',
+//  default: true,
+//  params: {
+//      key1: 'https://yanazhangabc.github.io',
+//      key2: 'value2',
+//  }
+//  },
+//  {
+//      el: '#openAppBtn1',
+//      path: 'yghsh-ios/CEBCREDIT/MobLinkViewController/a',
+//moblink_source_ios: '607e2b1363c0332d6516cd87',
+//      params: {
+//          key1: 'value1',
+//          key2: 'value2',
+//      }
+//  },
+//  {
+//      el: '#openAppBtn2',
+//       path: 'yghsh-ios/CEBCREDIT/MobLinkViewController/a',
+//moblink_source_ios: '607e2b1363c0332d6516cd87',
+//      params: {
+//          key1: 'value1',
+//          key2: 'value2',
+//      }
+//  }
+// ]);
