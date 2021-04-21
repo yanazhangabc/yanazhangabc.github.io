60a0955d733b09d3d7b10663c8243d3908976ced
张海彦 <yanazhangabc@126.com>
2021-04-21 14:33:12 +0800

    qq

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-// 页面上仅单个元素需要跳转时可以使用对象方式进行初始化
-MobLink({
-  el: '',
-  path: 'yghsh-ios/CEBCREDIT/MobLinkViewController',
-moblink_source_ios: '607e2b1363c0332d6516cd87',
-  default: true,
-  params: {
-      key1: 'https://yanazhangabc.github.io',
-      key2: 'value2',
-  }
- });
+//// 页面上仅单个元素需要跳转时可以使用对象方式进行初始化
+//MobLink({
+//  el: '',
+//  path: 'yghsh-ios/CEBCREDIT/MobLinkViewController',
+//moblink_source_ios: '607e2b1363c0332d6516cd87',
+//  default: true,
+//  params: {
+//      key1: 'https://yanazhangabc.github.io',
+//      key2: 'value2',
+//  }
+// });
 
 // // 页面上有多个元素需要跳转时要使用数组方式进行初始化
 //MobLink([
